7e4433f25ceabeb427494eff8d34bb0677f07c87
JuliusHG <julio.hdez.gc@gmail.com>
2024-01-28 01:22:52 -0600

    Pagination implemented
    I implemented pagination. Still "variable pagination" to go.

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
 import FirebaseConfig from "@/app/FirebaseConfig/FirebaseConfig";
 import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
@@ -7,29 +9,44 @@ import Link from "next/link";
 import Image from "next/image";
 import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import { ref, onValue } from "firebase/database";
-import { useState, useEffect } from "react";
-import { format } from 'date-fns';
 
 const database = FirebaseConfig();
 const dbRef = ref(database, "users");
 
 const UsersPage = () => {
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+  const [totalItems, setTotalItems] = useState(0);
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const [currentUsers, setCurrentUsers] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      onValue(dbRef, (snapshot) => {
-        const data = snapshot.val();
-        if (data) {
-          setUsers(Object.values(data));
-        } else {
-          setUsers([]);
+      onValue(
+        ref(database, "users"),
+        (snapshot) => {
+          const data = snapshot.val();
+          if (data) {
+            setUsers(Object.values(data));
+            setTotalItems(Object.keys(data).length);
+          } else {
+            setUsers([]);
+            setTotalItems(0);
+          }
+        },
+        {
+          orderByChild: "createdAt",
         }
-      });
+      );
     };
 
     fetchData();
-  }, [dbRef]);
+  }, [currentPage, database, itemsPerPage]);
+
+  useEffect(() => {
+    setCurrentUsers(users.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage));
+  }, [users, currentPage, itemsPerPage]);
 
   return (
     <div className={styles.container}>
@@ -51,37 +68,41 @@ const UsersPage = () => {
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
+          {currentUsers.map((user) => (
             <tr key={user.id}>
-                <td>
-                    <div className={styles.user}>
-                    <Image
-                        src="/noavatar.png"
-                        alt=""
-                        width={40}
-                        height={40}
-                        className={styles.userImage}
-                    />{" "}
-                    {user.name}
-                    </div>
-                </td>
-                <td>{user.email}</td>
-                <td>{format(new Date(user.createdAt), 'dd-MM-yyyy')}</td>
-                <td>{user.isAdmin ? 'Admin' : ''}</td>
-                <td>{user.isActive ? 'Active' : 'Inactive'}</td>
-                <td>
-                    <div className={styles.buttons}>
-                        <Link href={`/dashboard/users/${user.id}`}>
-                            <button className={`${styles.button} ${styles.view}`}>Ver</button>
-                        </Link>
-                        <button className={`${styles.button} ${styles.delete}`}>Eliminar</button>
-                    </div>
-                </td>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src="/noavatar.png"
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />{" "}
+                  {user.name}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{format(new Date(user.createdAt), "dd-MM-yyyy")}</td>
+              <td>{user.isAdmin ? "Admin" : ""}</td>
+              <td>{user.isActive ? "Active" : "Inactive"}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>Ver</button>
+                  </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>Eliminar</button>
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <Pagination/>
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalPages={totalPages}
+      />
     </div>
   );
 };
--- app/ui/dashboard/pagination/pagination.jsx
+++ app/ui/dashboard/pagination/pagination.jsx
@@ -1,11 +1,26 @@
 import styles from './pagination.module.css'
 
-const Pagination = () => {
+const Pagination = ({ currentPage, setCurrentPage, totalPages }) => {
     return (
         <div className={styles.container}>
-            <button className={styles.button} disabled>Anterior</button>
-            <button className={styles.button}>Siguiente</button>
-        </div>
+      <button
+        className={styles.button}
+        disabled={currentPage === 1}
+        onClick={() => setCurrentPage(currentPage - 1)}
+      >
+        Anterior
+      </button>
+      <span>
+        Página {currentPage} de {totalPages}
+      </span>
+      <button
+        className={styles.button}
+        disabled={currentPage === totalPages}
+        onClick={() => setCurrentPage(currentPage + 1)}
+      >
+        Siguiente
+      </button>
+    </div>
     )
 }
 
